bdf6779044735e6c52bfccfaaed91efbb6ddec8e
Magnus B <42898150+magnusBag@users.noreply.github.com>
2025-08-04 21:47:10 +0200

    remove old version

--- Visionsvej/updateMenu.ts
+++
@@ -1,8 +0,0 @@
-import { foodAndCoMenuParser } from "./foodAndCoMenuParser.ts";
-
-export async function updateMenu() {
-    const menu = await foodAndCoMenuParser();
-    console.log(menu);
-}
-
-updateMenu();
